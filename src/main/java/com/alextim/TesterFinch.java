3852e539444bc9bb472ccede7f48c744ba6bc842
AAdamyan0004 <aadamyan0004@mymail.lausd.net>
2026-05-28 14:08:22 -0700

    firstChangeInitial

---
+++ src/main/java/com/alextim/TesterFinch.java
@@ -0,0 +1,8 @@
+package.com.alextim;
+
+public class TesterFinch {
+    public static void main(String[] args){
+        FinalFinch damian = new FinalFinch("A");
+        myFinch.setMove("F",10,50);
+    }
+}
